308a3f863c3f62ea77fab88b5433c51398219157
Clara Calviño <mariaclaracalvino@gmail.com>
2023-05-18 16:34:37 -0300

    Agregado de archivo logica JS.Y funcionalidad de botones Compra de tickets.

---
+++ logica.js
@@ -0,0 +1,27 @@
+const divcaja = document.getElementById("caja");
+const divcaja2 = document.getElementById("caja2");
+const divcaja3 = document.getElementById("caja3");
+const divcaja4 = document.getElementById("caja4");
+const divcaja5 = document.getElementById("caja5");
+const divcaja6 = document.getElementById("caja6");
+
+const buttonbotonCompra = document.getElementById("botonCompra");
+const buttonbotonCompra2 = document.getElementById("botonCompra2");
+
+buttonbotonCompra.onclick=function () {
+    divcaja.style.display= "none";
+    divcaja2.style.display= "none";
+    divcaja3.style.display= "none";
+    divcaja4.style.display= "none";
+    divcaja5.style.display= "none";
+    divcaja6.style.display= "none";
+}
+
+buttonbotonCompra2.onclick=function () {
+    divcaja.style.display= "none";
+    divcaja2.style.display= "none";
+    divcaja3.style.display= "none";
+    divcaja4.style.display= "none";
+    divcaja5.style.display= "none";
+    divcaja6.style.display= "none";
+}
